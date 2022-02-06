d3e557b20975ffb9ce002f129e466affe0683f20
Chukwuebuka Valentine Okoye-Nobert <o.chukwuebu@alustudent.com>
2022-02-06 19:00:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,19 +22,24 @@ st.markdown(html_temp, unsafe_allow_html = True)
 
 # data= pd.read_csv('https://raw.githubusercontent.com/Nobert-Ok/Image_description_Clustering/main/train.csv')
 
-@st.cache  # No need for TTL this time. It's static data :)
-def get_data_by_state():
-	return pd.read_csv('https://raw.githubusercontent.com/Nobert-Ok/Image_description_Clustering/main/train.csv')
+# @st.cache  # No need for TTL this time. It's static data :)
+# def get_data_by_state():
+# 	return pd.read_csv('https://raw.githubusercontent.com/Nobert-Ok/Image_description_Clustering/main/train.csv')
 
-data = get_data_by_state()
-data['image_description'] = data['description']
-data = data[data['image_description'].notna()]
-documents = data['image_description'].values.astype("U")
-data['cluster'] = model.labels_
+# data = get_data_by_state()
+# data['image_description'] = data['description']
+# data = data[data['image_description'].notna()]
+# documents = data['image_description'].values.astype("U")
+# data['cluster'] = model.labels_
 
 
-clusters = data.groupby('cluster')  
-for cluster in clusters.groups:
-    data_clusters = clusters.get_group(cluster)[['categories','image_description']] # get title and overview columns
-    st.text('Cluster ' + str(cluster+1))
-    st.dataframe(data_clusters)
+# clusters = data.groupby('cluster')  
+# for cluster in clusters.groups:
+#     data_clusters = clusters.get_group(cluster)[['categories','image_description']] # get title and overview columns
+#     st.text('Cluster ' + str(cluster+1))
+#     st.dataframe(data_clusters)
+
+for cluster in range(1,6):
+    st.text('Cluster ' + str(cluster))
+    data = pd.read_csv('Cluster '+str(cluster)+'.csv')
+    st.dataframe(data)

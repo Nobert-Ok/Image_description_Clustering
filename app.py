c888fc6f48197ee037a430efb71874aade6400ca
Chukwuebuka Valentine Okoye-Nobert <o.chukwuebu@alustudent.com>
2022-02-06 11:46:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,14 +19,16 @@ st.markdown(html_temp, unsafe_allow_html = True)
 # default_value_goes_here = ""
 # Content = st.text_area("Please enter news from any source below: ", default_value_goes_here)
 # result =""
-data= pd.read_csv('https://raw.githubusercontent.com/NObert-Ok/Image_description_Clustering/main/train.csv')
+data= pd.read_csv('https://raw.githubusercontent.com/Nobert-Ok/Image_description_Clustering/main/train.csv')
 # data["label"] = label_enc.fit_transform(data[["label"]])  
 # when 'Predict' is clicked, make the prediction and store it 
 
+for i in range(len(3)):
+    st.text(str(i))
 
 
 documents = data['description'].values.astype("U")
-model.fit(tfid.fit_transform(documents))
+# model.fit(tfid.fit_transform(documents))
 
 # st.dataframe(pred)
 

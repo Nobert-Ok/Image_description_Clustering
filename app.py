a678a3a810666fbb5d7936c42d0b451c8600bfb8
Chukwuebuka Valentine Okoye-Nobert <o.chukwuebu@alustudent.com>
2022-02-06 15:57:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ model=pickle.load(open('kmeanmodel.pkl','rb'))
 # front end elements of the web page 
 html_temp = """ 
 <div style ="background-color:blue;padding:5px;"> 
-<h4 style ="color:black;text-align:center;">Image Description Text Clustering</h4> 
+<h1 style ="color:black;text-align:center;">Image Description Text Clustering</h1> 
 </div> 
-<h2>This web page displays clusters of product image description of different amazon products.
-The clustering is based on the similarity of the product description.
-</h2>
-<h1>Below are the 5 different clusters of the products</h1>
+<h4>This web page displays clusters of product image description of 45187 amazon products.
+The clustering is based on the similarity of the product image description.
+</h4>
+<h4>Below are the 5 different clusters of the products</h4>
 """ 
 st.markdown(html_temp, unsafe_allow_html = True) 
 
